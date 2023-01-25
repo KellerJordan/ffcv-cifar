0d753d541ba815f58bc99791a7b80047b3c1b2bd
KellerJordan <keller@kellerjordan.com>
2023-01-25 10:50:59 +0000

    write 10k too

--- write.py
+++ write.py
@@ -1,9 +1,12 @@
+import torch
 import torchvision
 from ffcv.fields import IntField, RGBImageField
 from ffcv.writer import DatasetWriter
 
+train_dset = torchvision.datasets.CIFAR10('/tmp', train=True, download=True)
 datasets = {
-    'train': torchvision.datasets.CIFAR10('/tmp', train=True, download=True),
+    'train': train_dset,
+    'train10k': torch.utils.data.Subset(train_dset, range(10000)),
     'test': torchvision.datasets.CIFAR10('/tmp', train=False, download=True)
 }
 
